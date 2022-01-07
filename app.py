e07057d4563a31f282c6aa4a85ec780e5cf08dbc
DanielaMazur <danielamazur2000@gmail.com>
2022-01-07 21:14:37 +0200

    remove function from app

--- app.py
+++ app.py
@@ -3,22 +3,20 @@ from flask_migrate import Migrate
 from models import db, company, calendar, employee, meeting, replacement_cost, role, training, turnover
 from apis import api, company, employee, role
 
-def create_app():
-    """Initialize the core application."""
-    app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:test@db/hrplus'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
-    api.init_app(app)
-    db.init_app(app)
+"""Initialize the core application."""
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:test@db/hrplus'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
-    app.run(debug=True)
-    # with app.app_context():
-    #     migrate = Migrate(app, db)
-    #     migrate.init_app(app)
-    #     return app
+api.init_app(app)
+db.init_app(app)
 
-create_app()
+if __name__ == "__main__":
+    app.run()
+    with app.app_context():
+        migrate = Migrate(app, db)
+        migrate.init_app(app)
 
 # source venv/Scripts/activate
 # python app.py
