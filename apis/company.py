e9276373460148ccff863bfd3c8afbb1b8008108
DanielaMazur <danielamazur2000@gmail.com>
2022-01-07 22:10:16 +0200

    Remove auth from company controller

--- apis/company.py
+++ apis/company.py
@@ -22,15 +22,15 @@ class CompanyList(Resource):
     @api.doc('create_company')
     @api.expect(createCompany)
     @api.marshal_with(company, code=201)
-    @cross_origin(headers=["Content-Type", "Authorization"])
-    @requires_auth
+    # @cross_origin(headers=["Content-Type", "Authorization"])
+    # @requires_auth
     def post(self):
         return companyDAO.create(api.payload)
         
     @api.doc('get_companies')
     @api.marshal_with(company, True, code=200)
-    @cross_origin(headers=["Content-Type", "Authorization"])
-    @requires_auth
+    # @cross_origin(headers=["Content-Type", "Authorization"])
+    # @requires_auth
     def get(self):
         return companyDAO.getAll()
 
@@ -40,13 +40,13 @@ class Company(Resource):
     @api.doc('update_company')
     @api.expect(createCompany)
     @api.marshal_with(company, code=200)
-    @cross_origin(headers=["Content-Type", "Authorization"])
-    @requires_auth
+    # @cross_origin(headers=["Content-Type", "Authorization"])
+    # @requires_auth
     def put(self, id):
         return companyDAO.update(id, api.payload)
 
     @api.doc('delete_company')
-    @cross_origin(headers=["Content-Type", "Authorization"])
-    @requires_auth
+    # @cross_origin(headers=["Content-Type", "Authorization"])
+    # @requires_auth
     def delete(self, id):
         return companyDAO.delete(id)
