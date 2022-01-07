892d57c8dddbf59cd80d8c7cdd3de9057f822bcc
DanielaMazur <danielamazur2000@gmail.com>
2022-01-07 22:09:10 +0200

    Temporarey remove auth from role controller

--- apis/role.py
+++ apis/role.py
@@ -26,22 +26,22 @@ class RoleList(Resource):
     @api.doc('create_role')
     @api.expect(createRole)
     @api.marshal_with(role, code=201)
-    @cross_origin(headers=["Content-Type", "Authorization"])
-    @requires_auth
+    # @cross_origin(headers=["Content-Type", "Authorization"])
+    # @requires_auth
     def post(self):
         return roleDAO.create(api.payload)
 
     @api.doc('get_roles')
     @api.marshal_with(role, True)
-    @cross_origin(headers=["Content-Type", "Authorization"])
-    @requires_auth
+    # @cross_origin(headers=["Content-Type", "Authorization"])
+    # @requires_auth
     def get(self):
         return roleDAO.getAll()
 
 @api.route('/<string:id>')
 class Role(Resource):
     @api.doc('delete_role')
-    @cross_origin(headers=["Content-Type", "Authorization"])
-    @requires_auth
+    # @cross_origin(headers=["Content-Type", "Authorization"])
+    # @requires_auth
     def delete(self, id):
         return roleDAO.delete(id)
